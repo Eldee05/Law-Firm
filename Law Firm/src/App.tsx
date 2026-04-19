df668562109280450b9b36ab3e98d310d8c695f5
Eldee05 <eldivinesolace@gmail.com>
2026-04-19 02:42:28 +0100

    File architectture: React with Vite and Tailwind

---
+++ Law Firm/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="items-center justify-center flex h-screen w-screen">
+      <h1 className="font-thin relative self-center text-2xl">
+        Hello World, It's time to build
+      </h1>
+    </div>
+  );
+}
+
+export default App;
